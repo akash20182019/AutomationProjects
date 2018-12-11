21ee1e3e2d97e92a3c4c732045b42628f13a3edc
akash20182019 <45789225+akash20182019@users.noreply.github.com>
2018-12-11 17:50:34 +0530

    Test Suites added
    Description of Test Suites

---
+++ Test Suites/TS_001.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-22T21:06:06</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b74e77b3-d033-4b3f-b3f7-b9a1f37d84d4</testSuiteGuid>
+   <testCaseLink>
+      <guid>523aa496-086b-43ed-83e8-38dc2452f783</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
